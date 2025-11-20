846e1f5b756be5de54598956e7bf0d5774e34cf3
hafiz <hafizzibrahimm0@gmail.com>
2025-11-20 08:00:29 +0700

    feat: update production database configuration and add migration for missing fields in 'ensiklopedia'

--- config/config.js
+++ config/config.js
@@ -20,7 +20,7 @@ module.exports = {
   production: {
     username: process.env.DB_USER,
     password: process.env.DB_PASS,
-    database: process.env.DB_NAME_PROD,
+    database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     dialect: 'mysql'
