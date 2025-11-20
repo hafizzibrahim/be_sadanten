9c7305a7ab1c104a9135c6c1942e535360f9d2e0
hafiz <hafizzibrahimm0@gmail.com>
2025-11-20 10:44:36 +0700

    feat: update database connection handling and change default port to 4000

--- server.js
+++ server.js
@@ -2,13 +2,16 @@ require('dotenv').config();
 const app = require('./app');
 const sequelize = require('./config/database');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
-sequelize.sync({ alter: true }).then(() => {
-  console.log('Database synced');
-  app.listen(PORT, () => {
-    console.log(`Server berjalan di http://localhost:${PORT}`);
+sequelize.authenticate()
+  .then(() => {
+    console.log('Koneksi ke database berhasil.');
+    app.listen(PORT, () => {
+      console.log(`Server berjalan di http://localhost:${PORT}`);
+    });
+  })
+  .catch(err => {
+    console.error('Gagal koneksi ke database:', err);
+    process.exit(1);
   });
-}).catch(err => {
-  console.error('Gagal koneksi ke database:', err);
-});
